da297c795c0c3cab6fd30765bcbc2138522c8eb3
Irina <iargina.irina@gmail.com>
2022-11-05 00:49:21 +0200

    task1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,49 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector(`.gallery`);
+const galleryList = document.createElement(`ul`);
+galleryList.classList.add(`gallery_list`);
+const link = "${item.preview}";
+gallery.append(galleryList);
+
+const galleryMass = galleryItems
+  .map(
+    (item) =>
+      `
+      <div class="gallery__item">
+  <a class="gallery__link" href="${item.original}">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      data-source="${item.original}"
+      alt="${item.description}"
+    />
+  </a>
+</div>`
+  )
+  .join("");
+galleryList.insertAdjacentHTML("beforeend", galleryMass);
+
+const photoOpen = (event) => {
+
+    if (event.target.nodeName !== "IMG") {
+        return;
+      }
+
+  const fullSize = event.target.closest(`.gallery__link`).getAttribute(`href`);
+  console.log(fullSize);
+
+  basicLightbox
+    .create(
+      `
+		<img src= ${fullSize}>
+	`
+    )
+    .show();
+
+  return;
+};
+galleryList.addEventListener(`click`, photoOpen);
