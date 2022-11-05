ff4858f644fec6bc635615f29203d81f95095c5c
Irina <iargina.irina@gmail.com>
2022-11-05 02:19:15 +0200

    task2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,33 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector(`.gallery`);
+const link = "${item.preview}";
+
+
+const galleryMass = galleryItems
+  .map(
+    (item) =>
+      `<a class="gallery__item" href="${item.original}">
+      <img class="gallery__image" src="${item.preview}" alt="${item.description}"/>
+    </a>`
+  )
+  .join("");
+
+  gallery.insertAdjacentHTML("beforeend", galleryMass);
+
+
+const photoOpen = (event) => {
+    event.preventDefault();
+    if (event.target.nodeName !== "IMG") {
+      return;
+    }
+        const lightbox = new SimpleLightbox('.gallery a', {captionsData: `alt`, captionDelay: 250});
+    return;
+  
+
+  };
+
+  gallery.addEventListener(`click`, photoOpen);
